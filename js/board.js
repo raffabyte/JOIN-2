ffbfce02ae64a32da92fec101c0b2f9d84c20cc1
abdosamo1 <136762539+abdosamo1@users.noreply.github.com>
2025-07-02 08:58:56 +0200

    Add overlay and task form to board with UI improvements
    Introduces an overlay modal for adding tasks on the board page, including new styles for overlays and forms. Refactors and enhances CSS for board layout, navigation, and scrollbar appearance. Updates HTML structure to support overlays and improves button and member display. Adds board.js for overlay logic and updates templates.js with overlay form rendering.

---
+++ js/board.js
@@ -0,0 +1,42 @@
+const OVERLAY = document.getElementById('overlay');
+const OVERLAY_CONTENT = document.getElementById('overlayContent');
+
+function addTaskOverlay() {
+   
+
+    // Set the overlay content to the add task form
+    OVERLAY_CONTENT.innerHTML = addTaskOverlayForm();
+    OVERLAY_CONTENT.classList.add('add-task');
+
+    // Show the overlay
+    OVERLAY.classList.remove('display-none');
+
+    setTimeout(() => {
+        // Add animation class to the overlay content
+        OVERLAY_CONTENT.classList.add('active');
+    }, 10);
+}
+
+
+
+function closeOverlay() {
+    // Remove animation class
+    OVERLAY_CONTENT.classList.remove('active');
+
+    // Hide the overlay after animation
+    setTimeout(() => {
+        OVERLAY.classList.add('display-none');
+        OVERLAY_CONTENT.classList.remove('add-task');
+        OVERLAY_CONTENT.innerHTML = '';
+    }, 110);
+}
+
+
+
+function closIFOverlay(event) {
+    // Check if the overlay is visible
+    if (event.target === OVERLAY) {
+        closeOverlay();
+    }
+}
+
